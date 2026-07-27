bbf59f4a18fbd82080d2524ac13b4fe5817f7673
Sandesh Mutadak <sandeshmurtadak@gmail.com>
2026-07-27 20:50:05 +0530

    Update Repo and Prosuctservice

--- ecom-proj/src/main/java/com/ecommerce/ecom_proj/repo/ProductRepo.java
+++ ecom-proj/src/main/java/com/ecommerce/ecom_proj/repo/ProductRepo.java
@@ -3,8 +3,19 @@ package com.ecommerce.ecom_proj.repo;
 import com.ecommerce.ecom_proj.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+import java.util.List;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product,Integer> {
 
+    @Query("SELECT p FROM Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(@Param("keyword") String keyword);
 }
--- ecom-proj/src/main/java/com/ecommerce/ecom_proj/service/ProductService.java
+++ ecom-proj/src/main/java/com/ecommerce/ecom_proj/service/ProductService.java
@@ -42,4 +42,7 @@ public class ProductService {
     public void deleteProduct(int id) {
         repo.deleteById(id);
     }
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProducts(keyword);
+    }
 }
