ab150f99bca3d049a25175a34decf61d369bb227
Sandesh Mutadak <sandeshmurtadak@gmail.com>
2026-07-28 17:46:02 +0530

    Update navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -107,9 +107,9 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
       <header>
         <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container-fluid">
-            <a className="navbar-brand" href="https://telusko.com/">
-              Telusko
-            </a>
+            <span className="navbar-brand">
+               ShopSphere
+             </span>
             <button
               className="navbar-toggler"
               type="button"
